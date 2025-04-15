825457bdef4fcebf1f3ef7241d58b6bda10906f1
lry22221111 <2247557140@qq.com>
2025-04-15 13:49:39 +0800

    [chore][sdk] Package the Python application to be compiled through brpc

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class CMakeBuild(build_ext):
             f"-DPYTHON_EXECUTABLE={sys.executable}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
             f"-DBUILD_PYTHON_SDK=ON",
-            f"-DSDK_ENABLE_GRPC=ON",
+            f"-DSDK_ENABLE_GRPC=OFF",
             f"-DBUILD_BENCHMARK=OFF",
             f"-DBUILD_INTEGRATION_TESTS=OFF",
             f"-DBUILD_UNIT_TESTS=OFF",
